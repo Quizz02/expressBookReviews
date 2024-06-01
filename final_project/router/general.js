0c72eff021ebb8f785af73a35dd0510aab1314a4
Quizz02 <u201910127@upc.edu.pe>
2024-06-01 01:10:01 -0400

    update user routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,43 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
   return res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+  return res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let filtered_books = []
+    for (let book in books){
+        if(books[book].author === author ){
+            filtered_books.push(books[book]);
+        }
+    }
+    res.send(filtered_books);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let filtered_books = []
+    for (let book in books){
+        if(books[book].title === title ){
+            filtered_books.push(books[book]);
+        }
+    }
+    res.send(filtered_books);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    return res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
